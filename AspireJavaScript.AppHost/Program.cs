b934fd289e9efd6a9b4eb83ab11f08d64fd2ccd8
Coen Hacking <coen.hacking@gmail.com>
2024-07-13 20:23:56 +0200

    Testing public URLs with YARP

--- AspireJavaScript.AppHost/Program.cs
+++ AspireJavaScript.AppHost/Program.cs
@@ -3,10 +3,18 @@
 var weatherApi = builder.AddProject<Projects.AspireJavaScript_MinimalApi>("weatherapi")
     .WithExternalHttpEndpoints();
 
-builder.AddNpmApp("react", "../AspireJavaScript.React")
+var react = builder.AddNpmApp("react", "../AspireJavaScript.React")
     .WithReference(weatherApi)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+var isHttps = builder.Configuration["DOTNET_LAUNCH_PROFILE"] == "https";
+var scheme = isHttps ? "https" : "http";
+
+builder.AddYarp("ingress")
+    .WithEndpoint(port: 8001, scheme: scheme)
+    .Route("weatherApi", path: "api.hello.cone.solutions/", target: weatherApi)
+    .Route("react", path: "hello.cone.solutions/", target: react);
 
 builder.Build().Run();
--- AspireJavaScript.MinimalApi/Program.cs
+++ AspireJavaScript.MinimalApi/Program.cs
@@ -30,6 +30,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => typeof(Program).AssemblyQualifiedName);
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
